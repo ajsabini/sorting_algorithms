f1b09e4e9fd6b70e38c2102336bebf6ac7cc5406
Alvaro <ajsabini@gmail.com>
2022-06-12 09:02:07 -0700

    0 + 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - ordenar (asc) una dll de int con insertion sort
+ * @list: la lista a ordenar
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	
+}
